77143c6f2c84156abb7076d1fb68455d6af3db75
Abdullah <48406637+bug-author@users.noreply.github.com>
2023-06-10 00:37:11 +0500

    Update publisher.js
    - migrated to redis v4
    - used readline since prompt-sync had issues in continuous console inputs

--- pubsub_redis/publisher.js
+++ pubsub_redis/publisher.js
@@ -1,9 +1,29 @@
-const redis = require("redis");
-const publisher = redis.createClient();
-const prompt = require("prompt-sync")({ sigint: true });
-var channel = "test";
-var message = prompt();
-message = "Nodejs : " + message;
-publisher.publish(channel, message, () => {
-  publisher.quit();
+const redis = require('redis');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
+
+const main = () => {
+  rl.question('', async (message) => {
+    if (message === 'exit') {
+      return rl.close();
+    }
+
+    const publisher = redis.createClient();
+    await publisher.connect();
+
+    const channel = 'test';
+    message = 'Nodejs : ' + message;
+
+    publisher.publish(channel, message, () => {
+      publisher.quit();
+    });
+
+    main();
+  });
+};
+
+main();
